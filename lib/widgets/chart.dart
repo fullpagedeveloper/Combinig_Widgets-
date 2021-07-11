43da4ba78e14f8b04ee13978796f73cc83be979e
fullpage_developer <60374847+fullpagedeveloper@users.noreply.github.com>
2021-07-12 00:21:03 +0700

    add
    adeed

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -38,7 +38,7 @@ class Chart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print("ahaha ====> $groupedTransactionValues");
+    print("ahah ====> $groupedTransactionValues");
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
