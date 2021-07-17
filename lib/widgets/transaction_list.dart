365a7efca6e0834628e8488324dabec673014496
Maulana Wes <maulanawest@arwics.com>
2021-07-18 01:23:02 +0700

    Calculating Size Dinamically

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -58,99 +58,96 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 450,
-      child: transaction.isEmpty
-          ? Column(
-              children: [
-                Text(
-                  "No Transactions added yet!",
-                  style: Theme.of(context).textTheme.title,
+    return transaction.isEmpty
+        ? Column(
+            children: [
+              Text(
+                "No Transactions added yet!",
+                style: Theme.of(context).textTheme.title,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 200,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
                 ),
-                SizedBox(
-                  height: 20,
+              ),
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              // return Card(
+              //   child: Row(
+              //     children: [
+              //       Container(
+              //         padding: EdgeInsets.all(
+              //           10,
+              //         ),
+              //         margin: EdgeInsets.symmetric(
+              //           vertical: 10,
+              //           horizontal: 15,
+              //         ),
+              //         decoration: BoxDecoration(
+              //           border: Border.all(
+              //             color: Theme.of(context).primaryColor,
+              //             width: 2,
+              //           ),
+              //         ),
+              //         child: Text(
+              //           ///toStringAsFixed(2) membulatkan bilangan
+              //           '\$${transaction[index].amount.toStringAsFixed(2)}',
+              //           style: TextStyle(
+              //             fontWeight: FontWeight.bold,
+              //             fontSize: 20,
+              //             color: Theme.of(context).primaryColor,
+              //           ),
+              //         ),
+              //       ),
+              //       Column(
+              //         crossAxisAlignment: CrossAxisAlignment.start,
+              //         children: [
+              //           Text(transaction[index].title),
+              //           Text(
+              //             DateFormat.yMMMEd().format(transaction[index].date),
+              //           ),
+              //         ],
+              //       ),
+              //     ],
+              //   ),
+              // );
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
                 ),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                // return Card(
-                //   child: Row(
-                //     children: [
-                //       Container(
-                //         padding: EdgeInsets.all(
-                //           10,
-                //         ),
-                //         margin: EdgeInsets.symmetric(
-                //           vertical: 10,
-                //           horizontal: 15,
-                //         ),
-                //         decoration: BoxDecoration(
-                //           border: Border.all(
-                //             color: Theme.of(context).primaryColor,
-                //             width: 2,
-                //           ),
-                //         ),
-                //         child: Text(
-                //           ///toStringAsFixed(2) membulatkan bilangan
-                //           '\$${transaction[index].amount.toStringAsFixed(2)}',
-                //           style: TextStyle(
-                //             fontWeight: FontWeight.bold,
-                //             fontSize: 20,
-                //             color: Theme.of(context).primaryColor,
-                //           ),
-                //         ),
-                //       ),
-                //       Column(
-                //         crossAxisAlignment: CrossAxisAlignment.start,
-                //         children: [
-                //           Text(transaction[index].title),
-                //           Text(
-                //             DateFormat.yMMMEd().format(transaction[index].date),
-                //           ),
-                //         ],
-                //       ),
-                //     ],
-                //   ),
-                // );
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8),
-                        child: FittedBox(
-                            child: Text('\$${transaction[index].amount}')),
-                      ),
-                    ),
-                    title: Text(
-                      transaction[index].title,
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    subtitle: Text(
-                        DateFormat.yMMMEd().format(transaction[index].date)),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => deleteTxt(transaction[index].id),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: FittedBox(
+                          child: Text('\$${transaction[index].amount}')),
                     ),
                   ),
-                );
-              },
-              itemCount: transaction.length,
-            ),
-    );
+                  title: Text(
+                    transaction[index].title,
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  subtitle: Text(
+                      DateFormat.yMMMEd().format(transaction[index].date)),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    onPressed: () => deleteTxt(transaction[index].id),
+                  ),
+                ),
+              );
+            },
+            itemCount: transaction.length,
+          );
   }
 }
