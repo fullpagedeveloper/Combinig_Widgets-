fb41d7933863a97d881ff06490ad1579a2e32821
Maulana Wes <maulanawest@arwics.com>
2021-07-26 00:47:37 +0700

    A Problem with Lists & Stateful Widgets

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,9 +1,11 @@
+import 'dart:math';
+
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 import 'package:flutter/material.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key key,
     @required this.transaction,
@@ -12,6 +14,26 @@ class TransactionItem extends StatelessWidget {
 
   final Transaction transaction;
   final Function deleteTxt;
+
+  @override
+  _TransactionItemState createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  Color _bgColor;
+
+  @override
+  void initState() {
+    const availableColor = [
+      Colors.red,
+      Colors.black,
+      Colors.blue,
+      Colors.purple
+    ];
+
+    _bgColor = availableColor[Random().nextInt(4)];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,31 +45,30 @@ class TransactionItem extends StatelessWidget {
       ),
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _bgColor,
           radius: 30,
           child: Padding(
             padding: const EdgeInsets.all(8),
-            child: FittedBox(
-                child: Text('\$${transaction.amount}')),
+            child: FittedBox(child: Text('\$${widget.transaction.amount}')),
           ),
         ),
         title: Text(
-          transaction.title,
+          widget.transaction.title,
           style: Theme.of(context).textTheme.title,
         ),
-        subtitle:
-        Text(DateFormat.yMMMEd().format(transaction.date)),
+        subtitle: Text(DateFormat.yMMMEd().format(widget.transaction.date)),
         trailing: MediaQuery.of(context).size.width > 360
             ? FlatButton.icon(
-          icon: Icon(Icons.delete),
-          label: Text('Delete'),
-          textColor: Theme.of(context).errorColor,
-          onPressed: () => deleteTxt(transaction.id),
-        )
+                icon: Icon(Icons.delete),
+                label: Text('Delete'),
+                textColor: Theme.of(context).errorColor,
+                onPressed: () => widget.deleteTxt(widget.transaction.id),
+              )
             : IconButton(
-          icon: Icon(Icons.delete),
-          color: Theme.of(context).errorColor,
-          onPressed: () => deleteTxt(transaction.id),
-        ),
+                icon: Icon(Icons.delete),
+                color: Theme.of(context).errorColor,
+                onPressed: () => widget.deleteTxt(widget.transaction.id),
+              ),
       ),
     );
   }
